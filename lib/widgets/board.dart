a243f2701d17be4f8dad14cbd114d0acd87ebb5a
Rohan Karn <rohankarn35@gmail.com>
2023-08-09 00:30:31 +0530

    Game Creaated

---
+++ lib/widgets/board.dart
@@ -0,0 +1,118 @@
+import 'package:eight_puzzle_game/widgets/tiles.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class board extends StatefulWidget {
+  const board({super.key});
+
+  @override
+  State<board> createState() => _boardState();
+}
+
+class _boardState extends State<board> {
+  var number = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  int moves = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    number.shuffle();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [Center(child: Tiles(onClick: onClick, number: number)),
+        SizedBox(height: 40,),
+        Text(
+                            "Moves: ${moves}",
+                            style: GoogleFonts.robotoSlab(
+                              fontSize: 25,
+                                color: Colors.black, fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(
+                            height: 100,
+                          ),
+                          Container(
+                            
+                            height: 50,
+                            width: 100,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(primary: Colors.deepPurpleAccent),
+                              
+                                    onPressed: shuffleGame,
+                                    child: Text("Shuffle",style: TextStyle(fontSize: 15, color: Colors.white),),
+                                  ),
+                          ),
+        ],
+      ),
+    );
+  }
+
+  void onClick(index) {
+    if (index - 1 > 0 && number[index - 1] == 0 && index % 3 != 0 ||
+        index + 1 < 9 && number[index + 1] == 0 && (index + 1) % 3 != 0 ||
+        (index - 3 >= 0 && number[index - 3] == 0) ||
+        (index + 3 < 9 && number[index + 3] == 0)){
+      setState(() {
+        number[number.indexOf(0)] = number[index];
+        number[index] = 0;
+        moves++;
+      });
+        }
+        checkWineer();
+  }
+  bool ishorted(List numberList){
+    int first = numberList.first;
+    for(int i = 1; i< numberList.length;i++){
+      int nextNumber = numberList[i];
+      if(first>nextNumber)
+        return false;
+        first = numberList[i];
+      
+    }
+    return true;
+  }
+  void checkWineer(){
+    bool isWinner = ishorted(number);
+    if(isWinner){
+      showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text(
+          "Winner",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w100,
+          ),
+        ));
+    }
+      );
+      setState(() {
+        number.shuffle();
+        moves = 0;
+      });
+    }
+  }
+  void shuffleGame(){
+    setState(() {
+      number.shuffle();
+    moves = 0;
+    });
+    
+  }
+  void showWinnerDialog(BuildContext context) {
+  
+
+
+    }
+}
--- lib/widgets/tiles.dart
+++ lib/widgets/tiles.dart
@@ -1,31 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Tiles extends StatelessWidget {
   var number = [];
 
-  Tiles({super.key, required this.onClick});
+  Tiles({super.key, required this.onClick, required this.number});
   Function onClick;
 
   @override
   Widget build(BuildContext context) {
- 
-
     return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.4,
+      width: MediaQuery.of(context).size.width * 0.2,
       child: GridView.builder(
-        itemCount: number.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-      
-      ), 
-      
-      itemBuilder: (context,index){
-        return number[index] != 0?TextButton(onPressed: (){
-          onClick(index);
-        }, 
-        child: Text(number[index])):const SizedBox.shrink();
-      }),
+          itemCount: number.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          ),
+          itemBuilder: (context, index) {
+            return number[index] != 0
+                ? GestureDetector(
+                  onTap: () {
+                    onClick(index);
+                  },
+                    child: Card(
+                      elevation: 5,
+                    child: Container(
+                      
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurpleAccent,
+                        borderRadius: BorderRadius.circular(8),
+                        
+                      ),
+                      height: 20,
+                      width: 20,
+                      child: Center(
+                        child: FittedBox(
+                          child: Text(
+                            number[index].toString(),
+                            style: GoogleFonts.robotoSlab(
+                              fontSize: 25,
+                                color: Colors.white, fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        
+                      ),
+                      
+                    ),
+                  ))
+                : const SizedBox.shrink();
+          }),
     );
   }
 }
